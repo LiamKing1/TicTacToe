faad58767eb0a74e44ac6bd34bb66199fd88bd0a
Liam King <techy.willyk3@gmail.com>
2023-11-21 14:15:54 -0600

    Ive gotten the app up and running with create react app, and have done a bit of styling setup and we are ready to build.

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1> Tic-Tac-Toe</h1>
+    )
+}
+
+export default Header;
